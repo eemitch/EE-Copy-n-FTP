38343697a15fe385f25053739952b112cba57391
Mitchell Bennis <48490506+eemitch@users.noreply.github.com>
2023-12-17 19:27:17 -0600

    Update eeCopyFile.php

--- eeCopyFile.php
+++ eeCopyFile.php
@@ -6,110 +6,106 @@
 // This script will display a simple form.
 // When the form is submitted it will use the inputs to copy a remote file to local.
 
-if (isset($_POST['eeGo'])) {
+<?php 
+// Mitchell Bennis (mitch@elementengage.com)
+// Version 3.0 -- Revamped for PHP 8 compatibility and best practices
 
-	if (isset($_POST['src_path'])) { // Check for source folder.
-		$src_path = $_POST['src_path'];
-	} else {
-		echo "<p>Forgot the Destination Folder!</p>";
-		exit();
+// This script displays a form and copies a remote file to a local destination upon submission.
+
+function urlExists($src) {
+	$handle = curl_init($src);
+	if (!$handle) {
+		throw new Exception("CURL initialization failed for URL: $src");
 	}
-	
-	if (isset($_POST['filename'])) { // Check for file name.
-		$filename = trim($_POST['filename']);
-	} else {
-		echo "<p>Forgot the File Name!</p>";
-		exit();
+	curl_setopt($handle, CURLOPT_HEADER, false);
+	curl_setopt($handle, CURLOPT_FAILONERROR, true);
+	curl_setopt($handle, CURLOPT_NOBODY, true);
+	curl_setopt($handle, CURLOPT_RETURNTRANSFER, false);
+	$result = curl_exec($handle);
+	if (curl_errno($handle)) {
+		throw new Exception("CURL error for URL: $src - " . curl_error($handle));
 	}
-	
-	if (isset($_POST['dest_folder'])) { // Check for destination folder.
-		$dest_folder = $_POST['dest_folder'];
-		$src = $src_path . '/' . $filename;
-	} else {
-		echo "<p>Forgot the Destination Folder!</p>";
-		exit();
-	}
-	
-	if ($_POST['rename']) { // Is rename selected?
-		if (isset($_POST['newname'])) { // Check for new name.
-			$newname = trim($_POST['newname']);
-		} else {
-			echo "<p>Forgot the New Name!</p>";
-			exit();
+	curl_close($handle);   
+	return $result;
+}
+
+function sanitize_input($data) {
+	return htmlspecialchars(stripslashes(trim($data)));
+}
+
+function process_input($field) {
+	return isset($_POST[$field]) ? sanitize_input($_POST[$field]) : null;
+}
+
+$src_path = $filename = $dest_folder = $newname = "";
+$error = "";
+
+try {
+	if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['eeGo'])) {
+		$src_path = process_input('src_path');
+		$filename = process_input('filename');
+		$dest_folder = process_input('dest_folder');
+		$rename = isset($_POST['rename']);
+		$newname = $rename ? process_input('newname') : $filename;
+
+		if (!$src_path || !$filename || !$dest_folder) {
+			throw new Exception("Required fields are missing.");
 		}
-	} else { // Keep same name.
-		$newname = $filename;
-	}
-	
-	
-	
-	$dest = $_SERVER['DOCUMENT_ROOT'] . '/' . $dest_folder . '/' . $newname;
-	
-	if (!url_exists($src)) { // Check for existing source file
-		echo '<p>ERROR: The source file does not exists!</p>';
-		echo '<p>' . $src . '</p>';
-		echo '<p>Try a different name.</p>';
-		exit();
-	}
-	
-	if (!is_dir($_SERVER['DOCUMENT_ROOT'] . '/' . $dest_folder)) { // Check for a destination folder.
-		echo '<p>ERROR: The destination location does not exists!</p>';
-		echo '<p>' . $_SERVER['DOCUMENT_ROOT'] . '/' . $dest_folder . '/' . '</p>';
-		echo '<p>Try a different path.</p>';
-		exit();
-	}
-	
-	if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/' . $dest_folder . '/' . $newname)) { // Check for existing destination file.
-		echo '<p>ERROR: This destination file already exists!</p>';
-		echo '<p>Use a different name.</p>';
-		exit();
-	}
-	
-	if (copy ($src, $dest)) {
+
+		if ($rename && !$newname) {
+			throw new Exception("New name for renaming is missing.");
+		}
+
+		$src = filter_var($src_path . '/' . $filename, FILTER_VALIDATE_URL);
+		if (!$src) {
+			throw new Exception("Invalid source URL.");
+		}
+
+		$dest = $_SERVER['DOCUMENT_ROOT'] . '/' . $dest_folder . '/' . $newname;
+
+		if (!url_exists($src)) {
+			throw new Exception("Source file does not exist: $src");
+		}
+
+		if (!is_dir($_SERVER['DOCUMENT_ROOT'] . '/' . $dest_folder)) {
+			throw new Exception("Destination folder does not exist: $dest_folder");
+		}
+
+		if (file_exists($dest)) {
+			throw new Exception("Destination file already exists: $newname");
+		}
+
+		if (!copy($src, $dest)) {
+			throw new Exception("Failed to copy file from $src to $dest");
+		}
+
 		echo "<p>Copy Complete: $newname</p>";
-		$filename = ''; // Clear the source file name. 
-		echo '<p>Copy Another File...</p>';
-		
-	} else {
-		echo "<p>COPY FAILED: File Not Found!</p>";
-		echo '<p>Please try again.</p>';
-		echo "<p>Source: $src</p>";
-		echo "<p>Destination: $dest</p>";
 	}
+} catch (Exception $e) {
+	$error = $e->getMessage();
 }
 ?>
 <!DOCTYPE HTML>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Copy Remote File - There to Here</title>
+	<meta charset="UTF-8">
+	<title>Copy Remote File - There to Here</title>
 </head>
 <body>
-<h1>Copy File</h1>
-<form action="copyFile.php" method="post">
-<p><strong>Source Path:</strong><input type="text" size="50" name="src_path" value="<?php echo $_POST['src_path']; ?>" /> /</p>
-<p><strong>Source File Name:</strong> <input type="text" size="45" name="filename" value="<?php echo $_POST['filename']; ?>" /></p>
-<hr width="50%" size="2" align="left" />
-<p><strong>Destination Folder:</strong> <input type="text" size="50" name="dest_folder" value="<?php if (isset($_POST['dest_folder'])) { echo $_POST['dest_folder']; } ?>" /> /</p>
-<p><input type="checkbox" name="rename" /> <strong>Rename at Destination to:</strong> <input type="text" size="40" name="newname" value="<?php if (isset($_POST['rename'])) { echo $newname; } ?>" /></p>
-<input type="submit" name="eeGo" value="Copy" />
-</form>
+	<h1>Copy File</h1>
+	<?php if ($error): ?>
+		<p style="color: red;"><?php echo $error; ?></p>
+	<?php endif; ?>
+	<form action="copyFile.php" method="post">
+		<p><strong>Source Path:</strong><input type="text" size="50" name="src_path" value="<?php echo $src_path; ?>" /> /</p>
+		<p><strong>Source File Name:</strong> <input type="text" size="45" name="filename" value="<?php echo $filename; ?>" /></p>
+		<hr width="50%" size="2" align="left" />
+		<p><strong>Destination Folder:</strong> <input type="text" size="50" name="dest_folder" value="<?php echo $dest_folder; ?>" /> /</p>
+		<p><input type="checkbox" name="rename" <?php if ($rename) echo "checked"; ?> /> <strong>Rename at Destination to:</strong> <input type="text" size="40" name="newname" value="<?php echo $newname; ?>" /></p>
+		<input type="submit" name="eeGo" value="Copy" />
+	</form>
 </body>
-</html><?<?php
+</html>
 
-function url_exists($src) { // Function to check source file.
-	$handle   = curl_init($src);
-	if (false === $handle) {
-		return false;
-	}
-	curl_setopt($handle, CURLOPT_HEADER, false);
-	curl_setopt($handle, CURLOPT_FAILONERROR, true);
-	curl_setopt($handle, CURLOPT_HTTPHEADER, Array("User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.15) Gecko/20080623 Firefox/2.0.0.15") ); // request as if Firefox    
-	curl_setopt($handle, CURLOPT_NOBODY, true);
-	curl_setopt($handle, CURLOPT_RETURNTRANSFER, false);
-	$connectable = curl_exec($handle);
-	curl_close($handle);   
-	return $connectable;
-}
 	
 ?>
